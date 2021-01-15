02076fa8ced48741537ef4231981f09ae29c2c4e
Lucas Lopes <lucasclopesr@gmail.com>
2021-01-14 21:12:07 -0300

    Adiciona avaliação das expressões

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,9 @@
 #include <string>
 #include <vector>
 #include <regex>
+#include <math.h>
 
-#define DEBUGGER true
+#define DEBUGGER false
 
 // Símbolos terminais:
 // std::regex RE_word("[a-zA-Z]*");
@@ -25,8 +26,9 @@
 // std::regex RE_unsigned_real("[0-9][0-9]*(\\.[0-9]+)?(E(\\+|-)?[0-9][0-9]*)?");
 std::regex RE_id("[a-zA-Z]([a-zA-Z]|[0-9])*");
 std::regex RE_constant("(\\+|-)?([0-9][0-9]*(\\.[0-9]+)?(E(\\+|-)?[0-9][0-9]*)?|[0-9][0-9]*)");
+std::regex RE_constant_exp("(\\+|-)?[0-9][0-9]*(\\.[0-9]+)?(E(\\+|-)[0-9][0-9]*)");
 std::regex RE_relop("(<=|>=|<>|=|<|>)(?![^(]*\\))(?!\\([^)]*)");
-std::regex RE_addop("\\b(\\+|-|or)(?![^(]*\\))");
+std::regex RE_addop("\\b(\\+|-|(or))(?![^(]*\\))");
 std::regex RE_mulop("(\\*|/|div|mod|and)(?![^(]*\\))");
 std::regex RE_fun("(sin|cos|log)");
 std::regex RE_parenthesis("(\\(*\\))");
@@ -34,6 +36,9 @@ std::regex RE_not("NOT");
 std::regex RE_sign("(\\+|-|or)");
 
 std::vector <int> evaluate_expr(std::string expr);
+
+// Vector containing a stack of symbols we should avaluate in case the expr is valid
+std::vector <std::string> calculate_vec;
 
 std::vector <std::string> split(std::string s){
   /* Quebra uma string que contém expressões separadas por vírgual em
@@ -78,6 +83,8 @@ std::vector <int> evaluate_factor(std::string factor){
       std::cout << "FUNCT EXPR match: " << op[0] << std::endl;
     }
 
+    calculate_vec.push_back(op[0]);
+
     size_t function_pos = factor.find(op[0]);
     factor.erase(function_pos, 3);
 
@@ -92,9 +99,9 @@ std::vector <int> evaluate_factor(std::string factor){
     std::string expr_in_parenthesis = factor.substr(open_par_pos+1, close_par_pos-open_par_pos-1);
 
     if (DEBUGGER) {
-      std::cout << "Function expr: " << factor << std::endl;
+      std::cout << "Function expr: " << expr_in_parenthesis << std::endl;
     }
-    std::vector <int> factor_code = evaluate_expr(factor);
+    std::vector <int> factor_code = evaluate_expr(expr_in_parenthesis);
 
     int_expr.push_back(FUNC_IDENTIFIER);
     int_expr.push_back(OPEN_P);
@@ -129,25 +136,39 @@ std::vector <int> evaluate_factor(std::string factor){
     int_expr.push_back(CLOSE_P);
 
     return int_expr;
-  }/* else if (std::regex_search(factor, op, RE_not)){
-    std::cout << "NOT  match: " << op[0] << std::endl;
-    return 0.0;
-  }*/ else if (std::regex_search(factor, op, RE_id)){
+  } else if (std::regex_search(factor, op, RE_constant_exp)){
     if (DEBUGGER) {
-      std::cout << "IDENTIFIER match: " << op[0] << std::endl;
+      std::cout << "EXP CONSTANT match: " << op[0] << std::endl;
     }
 
-    int_expr.push_back(IDENTIFIER);
+    calculate_vec.push_back(op[0]);
+    int_expr.push_back(CONSTANT);
 
     return int_expr;
   } else if (std::regex_search(factor, op, RE_constant)){
     if (DEBUGGER) {
       std::cout << "CONSTANT match: " << op[0] << std::endl;
     }
+
+    calculate_vec.push_back(op[0]);
     int_expr.push_back(CONSTANT);
 
     return int_expr;
-  } else {
+  } else if (std::regex_search(factor, op, RE_id)){
+    if (DEBUGGER) {
+      std::cout << "IDENTIFIER match: " << op[0] << std::endl;
+    }
+
+    std::string id_value;
+    std::cout << "Insira o valor de " << op[0] << ": ";
+    std::cin >> id_value;
+
+    calculate_vec.push_back(id_value);
+    int_expr.push_back(IDENTIFIER);
+
+    return int_expr;
+  }
+  else {
     if (DEBUGGER) {
       std::cout << "ERRO! SIMBOLO TERMINAL NAO RECONHECIDO" << std::endl;
     }
@@ -188,6 +209,8 @@ std::vector <int> evaluate_term(std::string term){
       std::cout << "MULOP match: " << op[1] << std::endl;
       std::cout << "MULOP length: " << op[1].length() << std::endl;
     }
+
+    calculate_vec.push_back(op[1]);
 
     size_t mulop_pos = term.find(op[1]);
     // simple expression before MULOP
@@ -255,6 +278,8 @@ std::vector <int>evaluate_simple_expr(std::string simple_expr){
       std::cout << "ADDOP match: " << op[1] << std::endl;
     }
 
+    calculate_vec.push_back(op[1]);
+
     size_t addop_pos = simple_expr.find(op[1]);
     // simple expression before ADDOP
     std::string se1 = simple_expr.substr(0,addop_pos);
@@ -294,6 +319,138 @@ float evaluate_relop(float se1, float se2, std::string relop){
   return 0.0;
 };
 
+float calculate_expr(){
+  std::string curr_symbol;
+
+  for (int i = calculate_vec.size()-1; i >= 0; i--){
+
+    curr_symbol = calculate_vec[i];
+
+    if (DEBUGGER) {
+      std::cout << "Symbol: " << curr_symbol << std::endl;
+    }
+
+    if (curr_symbol.compare("sin") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+
+      std::cout << "Resultado: " << sin(term1);
+    }
+    if (curr_symbol.compare("cos") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+
+      std::cout << "Resultado: " << cos(term1);
+    }
+    if (curr_symbol.compare("log") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+
+      std::cout << "Resultado: " << log(term1);
+    }
+    if (curr_symbol.compare("+") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+      float term2 = std::stof(calculate_vec[i+2]);
+
+      std::cout << "Resultado: " << term1+term2;
+    }
+    if (curr_symbol.compare("-") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+      float term2 = std::stof(calculate_vec[i+2]);
+
+      std::cout << "Resultado: " << term1-term2;
+    }
+    if (curr_symbol.compare("or") == 0){
+      bool term1 = std::stoi(calculate_vec[i+1]);
+      bool term2 = std::stoi(calculate_vec[i+2]);
+
+      std::cout << "Resultado: " << term1 || term2;
+    }
+    if (curr_symbol.compare("*") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+      float term2 = std::stof(calculate_vec[i+2]);
+
+      std::cout << "Resultado: " << term1*term2;
+    }
+    if (curr_symbol.compare("/") == 0 || curr_symbol.compare("div") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+      float term2 = std::stof(calculate_vec[i+2]);
+
+      std::cout << "Resultado: " << term1/term2;
+    }
+    if (curr_symbol.compare("mod") == 0){
+      int term1 = std::stof(calculate_vec[i+1]);
+      int term2 = std::stof(calculate_vec[i+2]);
+
+      std::cout << "Resultado: " << term1 % term2;
+    }
+    if (curr_symbol.compare("and") == 0){
+      bool term1 = std::stoi(calculate_vec[i+1]);
+      bool term2 = std::stoi(calculate_vec[i+2]);
+
+      std::cout << "Resultado: " << term1 && term2;
+    }
+    if (curr_symbol.compare("=") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+      float term2 = std::stof(calculate_vec[i+2]);
+
+      if (term1 == term2) {
+        std::cout << "Resultado: true";
+      } else {
+        std::cout << "Resultado: false";
+      }
+    }
+    if (curr_symbol.compare("<") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+      float term2 = std::stof(calculate_vec[i+2]);
+
+      if (term1 < term2) {
+        std::cout << "Resultado: true";
+      } else {
+        std::cout << "Resultado: false";
+      }
+    }
+    if (curr_symbol.compare("<=") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+      float term2 = std::stof(calculate_vec[i+2]);
+
+      if (term1 <= term2) {
+        std::cout << "Resultado: true";
+      } else {
+        std::cout << "Resultado: false";
+      }
+    }
+    if (curr_symbol.compare(">=") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+      float term2 = std::stof(calculate_vec[i+2]);
+
+      if (term1 >= term2) {
+        std::cout << "Resultado: true";
+      } else {
+        std::cout << "Resultado: false";
+      }
+    }
+    if (curr_symbol.compare(">") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+      float term2 = std::stof(calculate_vec[i+2]);
+
+      if (term1 > term2) {
+        std::cout << "Resultado: true";
+      } else {
+        std::cout << "Resultado: false";
+      }
+    }
+    if (curr_symbol.compare("<>") == 0){
+      float term1 = std::stof(calculate_vec[i+1]);
+      float term2 = std::stof(calculate_vec[i+2]);
+
+      if (term1 != term2) {
+        std::cout << "Resultado: true";
+      } else {
+        std::cout << "Resultado: false";
+      }
+    }
+  }
+  return 0.0;
+}
+
 std::vector <int> evaluate_expr(std::string expr){
 
   std::smatch op;
@@ -304,6 +461,8 @@ std::vector <int> evaluate_expr(std::string expr){
     if (DEBUGGER) {
       std::cout << "RELOP match: " << op[1] << std::endl;
     }
+
+    calculate_vec.push_back(op[1]);
 
     size_t relop_pos = expr.find(op[1]);
     // simple expression before RELOP
@@ -375,26 +534,31 @@ int main(){
           if (DEBUGGER) {
             std::cout << std::endl;
             for (auto & code : int_expr){
-              std::cout << code << std::endl;
+              std::cout << code;
             }
           }
 
           if(parser.parse(int_expr)){
-              std::cout << "Expressão válida." << std::endl;
+            // Avaliar expressão
+            std::cout << "Expressão válida." << std::endl;
+            
+            calculate_expr();
+
+            calculate_vec.clear();
           } else {
-              std::cout << "Expressão inválida." << std::endl;
+            throw "Expressão inválida";
           }
 
           std::cout << std::endl;
         } else {
-            std::cout << "Expressão inválida." << std::endl;
+            throw "Expressão inválida";
         }
 
       }
     }
     return 0;
 
-  } catch(std::exception& e){
+  } catch(...){
     std::cout << "Expressão inválida." << std::endl;
   }
 
